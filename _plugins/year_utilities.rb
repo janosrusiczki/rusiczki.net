0173783efa7ede725cc0542dc94e7f440450b19a
János Rusiczki <janos.rusiczki@gmail.com>
2023-10-03 12:44:48 +0300

    Archives page with year links

---
+++ _plugins/year_utilities.rb
@@ -0,0 +1,34 @@
+# {% year_utilities %}
+# Returns a list of links to all years that have posts in them
+#
+# To install, copy this file into your Jekyll/Octopress plugins folder
+# To use: {% year_utilities PARAMETER %}
+#
+# PARAMETER can be:
+# 	year_links (returns a list of links to all years that have posts in them)
+
+module Jekyll
+  class YearUtilities < Liquid::Tag
+    def initialize(tag, text, tokens)
+      super
+      @tag = tag
+      @text = text.strip
+      @tokens = tokens
+    end
+
+    def render(context)
+      the_result = nil
+
+      all_posts = context.registers[:site].posts.docs
+      years = all_posts.map { |post| post.date.year }.uniq
+
+      if @text.start_with? "year_links"
+        years.map { |year| "<a href='/#{year}/' class='year-link'>#{year}</a>" }.join(', ')
+      end
+
+      return the_result
+    end
+  end
+end
+
+Liquid::Template.register_tag('year_utilities', Jekyll::YearUtilities)
---
+++ archives.md
@@ -0,0 +1,7 @@
+---
+layout: page
+title: Archives
+---
+
+<h2>Yearly archives</h2>
+{% year_utilities year_links %}
